a34e7bfb1b5527812e58d0cbafeab26a34364fc3
daniel lissner <dlissner@gmail.com>
2014-09-20 17:15:52 -0500

    Update latest changes

--- js/cloudsynth.js
+++ js/cloudsynth.js
@@ -1,55 +1,115 @@
-setupSliders = function() {
-        $('.osc-frequency-slider').each(function(index, slider) {
-            $(slider).slider({
-              min         : 0.0,
-              max         : 1.0,
-              step        : 0.01,
-              orientation : 'vertical',
-              value       : 0.5,
-              handle      : 'round'
-            })
-            .on('slide', function(event) {
-              oscs[index].frequency.value = event.value * 2000;
-            });
-        });
+//wrapper for AudioNode objects to better manage
+//any-to-any routing
+var BreakoutNode = {
+  outputNodes : [],
 
-        $('.filter-frequency-slider').each(function(index, slider) {
-            $(slider).slider({
-              min         : 0.0,
-              max         : 1.0,
-              step        : 0.01,
-              orientation : 'vertical',
-              value       : 0.5,
-              handle      : 'round'
-            })
-            .on('slide', function(event) {
-              audiofilters[index].frequency.value = event.value * 2000;
-            });
-        });
+  //add a node to output to
+  connect     : function(outNode) {
+    var i;
 
-        $('.lfo-rate-slider').each(function(index, slider) {
-            $(slider).slider({
-              min         : 1,
-              max         : 10,
-              step        : 1,
-              orientation : 'vertical',
-              value       : 5,
-              handle      : 'round'
-            })
-            .on('slide', function(event) {
-              lfos[index].frequency.value = [
-              16,
-              8,
-              6,
-              4,
-              3,
-              2,
-              1,
-              0.5,
-              0.25,
-              0.0125
-            ][event.value];
+    //if already connected, ignore
+    if (this.outputNodes.indexOf(outNode))
+      return;
 
-            });
-        });
-      };
+    this.outputNodes.push(outNode);
+
+    this.node.disconnect();
+
+    for(i = 0; i < this.outputNodes.length; i++) {
+      this.node.connect(this.outputNodes[i]);
+    }
+  },
+
+  //disconnect all outputs
+  disconnect  : function() {
+    this.outputNodes = [];
+    this.node.disconnect();
+  },
+
+  disconnectNode  : function(outNode) {
+    var i,
+    position = this.outputNodes.indexOf(outNode);
+
+    //ignore if outNode not connected
+    if (!position)
+      return;
+
+    this.outputNodes.splice(position, 1);
+
+    this.node.disconnect();
+    for(i = 0; i < this.outputNodes.length; i++) {
+      this.node.connect(this.outputNodes[i]);
+    }
+  }
+};
+
+
+var setupSliders = function() {
+  $('.osc-frequency-slider').each(function(index, slider) {
+    $(slider).slider({
+      min         : 1.0,
+      max         : 0.0,
+      step        : 0.01,
+      orientation : 'vertical',
+      value       : 0.5,
+      handle      : 'round'
+    })
+    .on('slide', function(event) {
+      oscs[index].frequency.value = event.value * 2000;
+    });
+  });
+
+  $('.filter-frequency-slider').each(function(index, slider) {
+    $(slider).slider({
+      min         : 1.0,
+      max         : 0.0,
+      step        : 0.01,
+      orientation : 'vertical',
+      value       : 0.5,
+      handle      : 'round'
+    })
+    .on('slide', function(event) {
+      audiofilters[index].frequency.value = event.value * 2000;
+    });
+  });
+
+
+  $('.filter-lfo-amt-slider').each(function(index, slider) {
+    $(slider).slider({
+      min         : 1.0,
+      max         : 0.0,
+      step        : 0.01,
+      orientation : 'vertical',
+      value       : 0.5,
+      handle      : 'round'
+    })
+    .on('slide', function(event) {
+      audiofilters[index].frequency.value = event.value * 2000;
+    });
+  });
+
+  $('.lfo-rate-slider').each(function(index, slider) {
+    $(slider).slider({
+      min         : 10,
+      max         : 1,
+      step        : 1,
+      orientation : 'vertical',
+      value       : 5,
+      handle      : 'round'
+    })
+    .on('slide', function(event) {
+      lfos[index].frequency.value = [
+        16,
+        8,
+        6,
+        4,
+        3,
+        2,
+        1,
+        0.5,
+        0.25,
+        0.0125
+      ][event.value];
+    });
+  });
+};
